a874692f3604974981901fe6295d7dda0f59d99b
Andrew Goldstone <andrew.goldstone@gmail.com>
2026-05-01 14:35:11 -0400

    attempt to fix closing punctution for citeproc
    change csl to terminate with period
    little dumb filter to look for cites that end with a period or comma but are
    also followed by a period or comma and fix it.
    does okay with things like "Mrs. Dalloway and the Social System."
    Does not do okay with things like "Mrs. Dalloway," jork. Turns into
    "Mrs. Dalloway." jork. Let's try again another way.

--- memsyll-cite.lua
+++ memsyll-cite.lua
@@ -10,9 +10,10 @@
 
     See citeproc.sh for example usage.
 
-    TODO fix bad handling of closing punctuation
     TODO fix bad handling of multicites like [@cite1, @cite2]
 ]]
+
+local logging = require "logging"
 
 -- table of ids of references to add links to
 local to_link = { }
@@ -36,6 +37,28 @@ local function add_links(cite)
     end
 end
 
+-- cleanup CSL's mess when it comes to closing punctuation
+-- by looking for periods following a citation closing with a period or comma
+-- and removing them. It's not elegant but I think it works okay
+
+local quote = lpeg.S("\"'”’") -- curly & straight
+local close_punct = lpeg.S(",.") * quote^0 * -1 -- [,.]["'”’]*$
+local cleanup_pat = (1 - close_punct)^0 * close_punct -- complete pattern
+
+local function cleanup_postcite (il)
+    for i = #il, 1, -1 do
+        if il[i].t == "Cite" and il[i + 1] then
+            if cleanup_pat:match(pandoc.utils.stringify(il[i])) and
+                il[i + 1].t == "Str" and
+                close_punct:match(il[i + 1].text) then
+                il:remove(i + 1)
+            end
+        end
+    end
+
+    return il
+end
+
 function Pandoc(doc)
     -- verify output format option
     if PANDOC_WRITER_OPTIONS.extensions:find("citations") then
@@ -43,6 +66,7 @@ function Pandoc(doc)
         io.stderr:write("e.g. pandoc -t markdown-citations\n")
         return nil
     end
+
 
     local refs = pandoc.utils.references(doc)
     for ref in refs:iter() do
@@ -56,6 +80,9 @@ function Pandoc(doc)
     local citeproc_result = pandoc.utils.citeproc(doc)
 
     -- now put the urls in
-    return citeproc_result:walk { Cite = add_links }
+    return citeproc_result:walk {
+        Inlines = cleanup_postcite,
+        Cite = add_links
+    }
 end
 
